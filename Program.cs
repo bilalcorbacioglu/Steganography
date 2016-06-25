f29fd1c1512e2bdb9df474d044fa1c35300e0fac
Bilal <bilalcorbacioglu@gmail.com>
2016-06-25 04:15:20 +0300

    the code has been improved.

--- Program.cs
+++ Program.cs
@@ -9,35 +9,37 @@ namespace Steganography
 {
     class Program
     {
-	//Resimlerin Path'i derken .txt olacak resimler.Bunun icin Secret Sharing deki bmptotxt.cpp kullanabilirsiniz.
-        static int totaline(int next) { 
-			StreamReader TL = new StreamReader("");//Gomulecek resmin yolu.(Sacma resim mesela.)
-                string line = TL.ReadLine();
-                while (line != null)
-                {
-                    
-                    //Console.WriteLine(line);
-                    next = next + 1;
-                    line = TL.ReadLine();
-                        
-                }
-                TL.Close();
-                Console.WriteLine(next);
-           return next;
+        static int totaline(int next,string hideImage) { 
+            StreamReader TL = new StreamReader(hideImage);
+            string line = TL.ReadLine();
+            while (line != null)
+            {
+                next = next + 1;
+                line = TL.ReadLine();
+            }
+            TL.Close();
+            return next;
         }
 
         static void Main(string[] args)
         {
+            //hideImage and carriersImage
+            Console.Write("The image you want to hide, enter the path. (Note: It should be more small)\n");
+            string hideImage = Console.ReadLine();
+
+            Console.Write("Carriers of the image, enter the path. (Note: The image must be large)\n");
+            string carriersImage = Console.ReadLine();
+
+
             int next = 0;
-            int subline = totaline(next) * 4;
+            int subline = totaline(next,hideImage) * 4;
             string[] array = new string[subline];
             int length = 0, fark = 0, j = 0, i = 0, k = 0, f = 0;
-			StreamReader SRG = new StreamReader("");//Gomulecek resmin yolu.(Sacma resim mesela.)
-			StreamWriter WR = new StreamWriter("");//Gomulmus txt nin kaydolacagi yer ardindan Secret Sharing deki txttobmp.cpp kullanabilirsiniz.
+            StreamReader SRG = new StreamReader(hideImage);
+            StreamWriter WR = new StreamWriter("./CompletedPictures.txt");
             string line1 = SRG.ReadLine();
             while (line1 != null)
             {
-                //Console.WriteLine(line1);
                 string stringbin = Convert.ToString(Convert.ToInt32(line1, 10), 2);
                 fark = 0;
                 length = stringbin.Length;
@@ -50,10 +52,7 @@ namespace Steganography
                     };
                     j = 0;
                 }
-                line1 = SRG.ReadLine();//Ilk line while disinda okudugumuz icin ilk satiri isleme sokmamiz gerekiyor.
-                //WR.WriteLine(stringbin);
-                //Console.WriteLine(stringbin);
-                //WR.Flush();
+                line1 = SRG.ReadLine();
                 for (i = 0; i < 4; i++ ){
                     array[f] = stringbin.Substring(k,2);
                     k = k + 2;
@@ -61,16 +60,12 @@ namespace Steganography
                 }
                 k = 0;
             }
-            //WR.Close();
-
             string[] arraygom = new string[subline];
-			StreamReader SR = new StreamReader("");//Icine gomulecek resim yolu
+            StreamReader SR = new StreamReader(carriersImage);
             string line2 = SR.ReadLine();
-
             int z = 0;
             while (line2 != null)
             {
-                //Console.WriteLine(line2);
                 string stringbin = Convert.ToString(Convert.ToInt32(line2, 10), 2);
                 fark = 0;
                 length = stringbin.Length;
@@ -83,17 +78,17 @@ namespace Steganography
                     };
                     j = 0;
                 }
-                //Bu noktada strinbin bizim decimal to binary degerimiz 
+                //Decimal to Binary 
                 line2 = SR.ReadLine();
                 if ( z < subline ) {
                     arraygom[z] = stringbin.Substring(0, 6) + array[z];
-					long l = Convert.ToInt64 (arraygom[z], 2);
-					WR.WriteLine(l);
-					WR.Flush();
+                    long l = Convert.ToInt64 (arraygom[z], 2);
+                    WR.WriteLine(l);
+                    WR.Flush();
                     z++;
                 }
                 else {
-					long l = Convert.ToInt64(stringbin,2);
+                    long l = Convert.ToInt64(stringbin,2);
                     WR.WriteLine(l);
                     WR.Flush();
                     z++;
@@ -101,8 +96,7 @@ namespace Steganography
             }
 
             WR.Close();
-                Console.Read();
-        
+            Console.WriteLine("Completed");
         }
     }
 }
